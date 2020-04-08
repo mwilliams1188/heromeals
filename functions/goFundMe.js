412c81776f3412e487de1f16ff55e085d3397a1e
Michael Williams <mwilliams1188@gmail.com>
2020-04-08 00:00:52 -0400

    async

--- functions/goFundMe.js
+++ functions/goFundMe.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 const url = 'https://hook.integromat.com/b6p8mibmv629yrxya8u374h1pn8k9nik'
 
-exports.handler = function(event, context, callback) {
+exports.handler = async function(event, context, callback) {
   let response = await fetch(url).then(res => res.json())
   console.log(response)
 
--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,7 +2,7 @@
   <div class="text-gray-600">
     🖥️ by <a href="https://mwilliams.co" target="_blank" class="font-semibold text-blue-700 hover:text-blue-800 transition duration-150">Mike Williams</a>
     @
-    <a href="https://buildlab.co" target="_blank" class="font-semibold text-blue-700 hover:text-blue-800 transition duration-150">BuildLab Digital</a>
+    <a href="https://buildlab.co" target="_blank" class="font-semibold text-blue-700 hover:text-blue-800 transition duration-150">BuildLab</a>
   </div>
   <!-- <div class="mt-4">
     <p class="text-center text-base leading-6 text-green-100 font-semibold">
